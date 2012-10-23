0cd533a2f22f21facbe7728c11074a8e63e2455d
Pierre Marchand <pierre@oep-h.com>
2012-10-23 11:53:11 +0200

    Added observers setup to install_map

--- pg_bridge/pgbridge.py
+++ pg_bridge/pgbridge.py
@@ -54,7 +54,7 @@ class PGBMABridge(object):
         polygon = 'POLYGON(('+ ','.join([c0,c1,c2,c3,c0]) +'))'
         st_polygon = "ST_GeomFromText('" + polygon + "', "+ str(srid) +")"
         query = 'SELECT id FROM %s WHERE ST_Contains(ST_Transform(%s,ST_SRID(%s)), %s)'%(self.layer, st_polygon, self.geometry_col, self.geometry_col)
-        print('[RECT] %s'%query)
+        #print('[RECT] %s'%query)
         self.cursor.execute(query)
         ret = []
         for row in self.cursor.fetchall():
